55a90865d95fc2e2957095638d1e229eb69f10ec
Tyler Gilchrist <tyler668@gmail.com>
2020-05-18 15:57:01 -0400

    Sort / search MVP + stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,25 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        # while cur_index >= smallest_index:
+        #     cur_index += 1
+        # smallest_index = cur_index
+        for j in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j              
+                
 
 
         # TO-DO: swap
         # Your code here
+        arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
+
 
     return arr
 
@@ -18,6 +27,17 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    swaps = 0
+
+    while swaps is not None:
+        swaps = 0
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                swaps += 1
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        if swaps == 0:
+            swaps = None
+
 
 
     return arr
